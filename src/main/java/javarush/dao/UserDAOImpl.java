df51703a116294b4010b82913c101a7531f62226
Your Name <you@example.com>
2017-02-15 13:54:03 +0300

    Imolements UserDAO

--- src/main/java/javarush/dao/UserDAOImpl.java
+++ src/main/java/javarush/dao/UserDAOImpl.java
@@ -1,7 +1,38 @@
 package javarush.dao;
 
-/**
- * Created by fanta on 14.02.2017.
- */
-public class UserDAOImpl {
+import javarush.model.User;
+
+import java.util.List;
+
+public class UserDAOImpl implements UserDAO {
+
+    @Override
+    public void addTeam(User team) {
+
+    }
+
+    @Override
+    public void updateTeam(User team) {
+
+    }
+
+    @Override
+    public User getTeam(int id) {
+        return null;
+    }
+
+    @Override
+    public void deleteTeam(int id) {
+
+    }
+
+    @Override
+    public User searchUser(String name) {
+        return null;
+    }
+
+    @Override
+    public List<User> getTeams() {
+        return null;
+    }
 }
